eb8b08c4a10dc42ae91ee974002d4d17ce870dd9
ghaz0 <98414281+ghaz0@users.noreply.github.com>
2022-12-05 19:37:40 -0500

    Added better comments

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ def main():
 	if len(inputString) > 40 or len(inputString) == 0:
 		print("Error: String too long or too short!")
 		return
+	
+	# determine "buffer" by taking the difference b/w input length and
+	# 40 chars (display_panel length) and dividing it by two, one half for
+	# the front and the other for the end for a "centered" position, rounding
+	# down with int division for odd-count input string lengths
 	spaceBuffer = (40 - len(inputString))//2
 	
+	# iterate starting at start buffer position
 	counter = 0
 	for index in range(spaceBuffer, spaceBuffer + len(inputString)):
 		display_panel[index] = inputString[counter]
